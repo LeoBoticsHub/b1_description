d39fb5e4b71508e5bd7a279a2951326ac5504544
Federico Rollo <rollo.f96@gmail.com>
2024-04-11 13:32:15 +0200

    adding condition to rviz node

--- launch/upload.launch.py
+++ launch/upload.launch.py
@@ -1,18 +1,28 @@
 from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-from launch.substitutions import Command
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
+
+from ament_index_python.packages import get_package_share_directory
+
 import xacro
 import os
-from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
 
+    # Args
+    use_rviz = LaunchConfiguration('use_rviz')
+
+    DeclareLaunchArgument(
+        'use_rviz',
+        default_value='False'
+    )
+
+    # B1 description loading from xacro
     pkg_path = get_package_share_directory("b1_description")
-
     xacro_path = os.path.join(pkg_path, 'xacro/robot.xacro')
-
     assert os.path.exists(xacro_path), "The robot.xacro doesnt exist in " + str(xacro_path)
-
     doc = xacro.process_file(xacro_path, mappings={'DEBUG': 'false', 'SENSORS': 'true',  'LIDAR': 'true', 'CAMERAS': 'true'})
     robot_desc = doc.toprettyxml(indent='  ')
 
@@ -27,11 +37,12 @@ def generate_launch_description():
     )
 
     rviz = Node(
-            package='rviz2',
-            namespace='',
-            executable='rviz2',
-            name='rviz2',
-            arguments=['-d', [os.path.join(pkg_path, 'rviz', 'ros2_robot_description.rviz')]]
+        condition=IfCondition(use_rviz),
+        package='rviz2',
+        namespace='',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', [os.path.join(pkg_path, 'rviz', 'ros2_robot_description.rviz')]]
     )
     
     return LaunchDescription([robot_state_pub, rviz])
