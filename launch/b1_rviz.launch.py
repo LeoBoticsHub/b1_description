eec8947a8ec384c12ded571996861aa1a78c3100
Valentina <valentina.pericu@gmail.com>
2025-06-18 17:17:57 +0200

    [minor] typo

--- launch/b1_rviz.launch.py
+++ launch/b1_rviz.launch.py
@@ -11,7 +11,7 @@ def generate_launch_description():
 
     xacro_path = os.path.join(pkg_path, 'xacro/robot.xacro')
 
-    assert os.path.exists(xacro_path), "The robot.xacro doesnt exist in " + str(xacro_path)
+    assert os.path.exists(xacro_path), "The robot.xacro doesn't exist in " + str(xacro_path)
 
     doc = xacro.process_file(xacro_path, mappings={'DEBUG': 'false', 'SENSORS': 'true',  'LIDAR': 'true', 'CAMERAS': 'true'})
     robot_desc = doc.toprettyxml(indent='  ')
